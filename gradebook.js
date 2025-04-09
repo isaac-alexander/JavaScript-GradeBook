3b2f577f63f3d5c59d2484606f0a6f503f3b4fed
Isaac Alexander <Isaacalexander81@gmail.com>
2025-04-09 01:43:17 +0100

    gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -1,35 +1,52 @@
 function getAverage(scores) {
     let sum = 0;
 
-    for (const score of scores) {
-        sum += score;
+    for (const score of scores) { //92, 88, 12, 77, 57, 100, 67, 38, 97, 89
+        console.log(score);
+
+        sum += score; //sum = 92,sum = 180,sum = 192,sum = 269,sum = 326,sum = 426,sum = 493,sum = 531,sum = 628,sum = 717
+        console.log(`this is sum ${sum}`);
+        console.log(scores.length);
+
     }
 
-    return sum / scores.length;
+
+    return sum / scores.length; // 71.7
 }
 
-function getGrade(score) {
-    if (score === 100) {
+function getGrade(score) { //37
+    console.log(`this is the function score ${score}`);
+
+    console.log(`score is ${score === 100}`);
+
+    if (score === 100) { //false 
         return "A++";
+
     } else if (score >= 90) {
         return "A";
+
     } else if (score >= 80) {
         return "B";
+
     } else if (score >= 70) {
         return "C";
+
     } else if (score >= 60) {
         return "D";
     } else {
-        return "F";
+        return "F"; //true
     }
 }
 
-function hasPassingGrade(score) {
-    return getGrade(score) !== "F";
+function hasPassingGrade(score) {    
+    return getGrade(score) !== "F"; //false
 }
+
 function studentMsg(totalScores, studentScore) {
-    let average = getAverage(totalScores);
-    let grade = getGrade(studentScore);
+    let average = getAverage(totalScores); //71.7
+
+    let grade = getGrade(studentScore); //37 or F
+
 
     return "Class average: " + average + ". Your grade: " + grade + ". You " +
         (hasPassingGrade(studentScore) ? "passed" : "failed") + " the course.";
